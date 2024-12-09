9d2c7ddd0eab85c063419d96c7a22fea4d8a56a4
Nikita <nikitatewari1633@gmail.com>
2024-12-09 22:36:25 +0530

    Update index.js
    adding data in database

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const mongoose = require("mongoose")
+const studentModel= require("./models/student.model")
 
 mongoose.connect("mongodb://localhost/be_demodb") //where to connect
 const db= mongoose.connection //start the connection with mongo database
 db.on("error", ()=>{
     console.log("Error while Connecting")
+    
 });
 db.once("open", ()=>{
     console.log("Connected to Mongo database")
+    //logic to insert data into the db 
 });
+async function init(){
+    const student={
+        name: "Nikita",
+        age: 21
+    }
+   const std_obj=  await studentModel.create(student)
+   console.log(std_obj)
+}
